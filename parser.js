4c5b1f5a615a9f5fc563d85e9a584ee1615325f2
Emanuele Ricci <stermi@gmail.com>
2013-04-14 15:35:35 +0200

    updated parser

--- parser.js
+++ parser.js
@@ -1,26 +1,41 @@
 fs = require('fs');
 libxmljs = require('libxmljs');
 
-function parseForImport( fileName ) {
-	fs.readFile(fileName, 'utf8', function(err, data) {
+function parseForImport( inputFile, outputFile ) {
+	fs.readFile(inputFile, 'utf8', function(err, data) {
 		if (err) {
 			return console.log(err);
 		} else {
 			var xmlDoc = libxmljs.parseXmlString(data);
-			var strings = xmlDoc.find('//string');
-			for( var i = 0; i < strings.length; i++ ) {
-				var value = strings[i].text();
+			var resources = xmlDoc.get('//resources');
+			var childs = resources.childNodes();
+
+			//Creating new document
+			var newDoc = new libxmljs.Document();
+			var resourcesNode = newDoc.node('resources');
+			resourcesNode.attr('xmlns:xliff', 'urn:oasis:names:tc:xliff:document:1.2');
+
+			for( var i = 0; i < childs.length; i++ ) {
+				var value = childs[i].text();
 				value = value.replace( /^[\"]?(.*?)[\"]?$/g, "$1" );
-				console.log( strings[i].attr('name').value() + ' -> ' + value );
-
-
+				//console.log( childs[i].attr('name').value() + ' -> ' + value );
+				childs[i].text(value);
+				resourcesNode.addChild( childs[i] );
 				//%s -> %1$s
 			}
+
+			fs.writeFile( outputFile, newDoc.toString(), function(err) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("The file was saved!");
+				}
+			});
 		}
 	});
 }
 
-function parseForExport( inputFile, outputFile) {
+function parseForExport( inputFile, outputFile ) {
 	fs.readFile(inputFile, 'utf8', function(err, data) {
 		if (err) {
 			return console.log(err);
@@ -54,5 +69,5 @@ function parseForExport( inputFile, outputFile) {
 
 
 //Examples usage
-//parseForImport('./mxm_android_de.xml');
-parseForExport('./strings.xml', './out.xml');
+//parseForImport('./mxm_android_de.xml', './mxm_android_de_out.xml');
+parseForExport('./strings.xml', './strings-out.xml');
